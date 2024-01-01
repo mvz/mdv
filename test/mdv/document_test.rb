f1584fff2dd7d348783977a54573c99fc7feb4c2
Matijs van Zuijlen <matijs@matijs.net>
2024-01-01 11:06:26 +0100

    Replace github-markdown with commonmarker
    The github-markdown gem is no longer maintained. To make things work
    with commonmarker, its version has to be fixed at 0.23, since
    github-markup expectes commonmarker to provide the CommonMarker module,
    whereas commonmarker 1.0 provides Commonmarker instead.
    
    There is a small change in the number of newlines output in the
    resulting HTML, but this is not a structural change.

--- test/mdv/document_test.rb
+++ test/mdv/document_test.rb
@@ -12,7 +12,7 @@ describe MDV::Document do
   describe "#html" do
     it "renders the correct html" do
       _(document.html)
-        .must_equal "<h1>Test</h1>\n\n<p>This is a test document.</p>\n"
+        .must_equal "<h1>Test</h1>\n<p>This is a test document.</p>\n"
     end
 
     it "renders the correct html for this project's README" do
